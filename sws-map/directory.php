e9d8b140cf726cef62b4c501184e952aa031f775
Sharon Stromberg <sharon@ccharacter.com>
2021-07-23 18:01:37 -0400

    fdsa

--- sws-map/directory.php
+++ sws-map/directory.php
@@ -3,8 +3,8 @@
 
 echo getcwd();
 
-require_once '../../inc/dir/assets/Db.php';
-require_once '../../inc/dir/assets/dir_functions.php';
+require_once '/../inc/dir/assets/Db.php';
+require_once '/../inc/dir/assets/dir_functions.php';
 
 
 $_SESSION['sew']['which']="cm";
