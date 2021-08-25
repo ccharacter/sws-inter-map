69a97ed54d635f257d7c5828c1e98f25d3b9d3b3
Sharon Stromberg <sharon@ccharacter.com>
2021-08-25 11:38:28 -0400

    log sql

--- inc/dir/assets/dir_functions.php
+++ inc/dir/assets/dir_functions.php
@@ -249,7 +249,7 @@ function ejj_list_dir_by_union($unionCode) {
 
 	echo "<h3>$ministry Leadership in the $union</h3><div class='dirlist_div'>";
 	
-	$sql="select * from dbi_master where $groupCond and union_conf like '".$union."%' and (conference like '%".$union."%' or conference='' or conference like '%Union%' or conference is null)";  //echo $sql;
+	$sql="select * from dbi_master where $groupCond and union_conf like '".$union."%' and (conference like '%".$union."%' or conference='' or conference like '%Union%' or conference is null)";  error_log($sql,0);
 
 	
 	$union_array = $db -> select($sql); 
